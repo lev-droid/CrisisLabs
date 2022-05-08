1e1253a3746a426b3b658696f5613d2521033a00
sexmaster226 <smithan@whs.school.nz>
2022-05-09 01:08:26 +1200

    Added vertical zoom to graph
    Added a vertical zoom bar, and created a programming abomination in the name of aesthetics.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var GraphContext;
 var Graph;
 
+var riskColour = "#9999ff";
+
 const GRAPHSIZE = 384;
 const BORDERTHICKNESS = 4;
 
@@ -91,14 +93,17 @@ function drawGraph(graph, ctx, colour, size, data, displayAmount, minY, maxY)
 	var vertLinesDistance = Math.ceil(cDisplayAmount/15); // Distance between labels on vertical lines, in units
 	for (var i = Math.ceil(metaData[0]); i < metaData[1]; i += 1)
 	{
+		var cColour = "#505050";
 		var nX = stretchNum(i, metaData[0], metaData[1], GRAPHMARGINX, GRAPHSIZE-GRAPHMARGINX);
-		drawLine(ctx, "#505050", nX, GRAPHMARGINY, nX, GRAPHSIZE-GRAPHMARGINY, GRAPHLINESIZE);
-		
+
 		if (i % vertLinesDistance == 0) {
-			ctx.fillStyle = "#ffffff";
+			/*ctx.fillStyle = "#ffffff";
 			ctx.font = "10px Monospace";
-			ctx.fillText(i, nX - (i.toString().length)*3, GRAPHMARGINY-5); 
+			ctx.fillText(i, nX - (i.toString().length)*3, GRAPHMARGINY-5);*/
+			cColour = "#606060";
 		}
+		
+		drawLine(ctx, cColour, nX, GRAPHMARGINY, nX, GRAPHSIZE-GRAPHMARGINY, GRAPHLINESIZE);
 	}
 	
 	// Horizontal lines
@@ -106,18 +111,20 @@ function drawGraph(graph, ctx, colour, size, data, displayAmount, minY, maxY)
 	for (var i = Math.ceil(metaData[2]); i < metaData[3]; i += 1)
 	{
 		var cColour = "#505050";
+		var nY = stretchNum(i, metaData[2], metaData[3], GRAPHMARGINY, GRAPHSIZE-GRAPHMARGINY);
+		
+		if (i % horizLinesDistance == 0) {
+			/*ctx.fillStyle = "#ffffff";
+			ctx.font = "10px Monospace";
+			ctx.fillText(i, GRAPHSIZE-GRAPHMARGINX+(i.toString().length)+3, nY+3);*/
+			cColour = "#606060";
+		}
+		
 		if (i == 0) {
 			cColour = "#808080";
 		}
 		
-		var nY = stretchNum(i, metaData[2], metaData[3], GRAPHMARGINY, GRAPHSIZE-GRAPHMARGINY);
 		drawLine(ctx, cColour, GRAPHMARGINX, nY, GRAPHSIZE-GRAPHMARGINX, nY, GRAPHLINESIZE);
-		
-		if (i % horizLinesDistance == 0) {
-			ctx.fillStyle = "#ffffff";
-			ctx.font = "10px Monospace";
-			ctx.fillText(i, GRAPHSIZE-GRAPHMARGINX+(i.toString().length)+3, nY+3);
-		}
 	}
 	
 	// old x/y, because drawing lines between points requires the positions of both the current point and the previous one
@@ -139,10 +146,47 @@ function drawGraph(graph, ctx, colour, size, data, displayAmount, minY, maxY)
 		oY = cY;
 	}
 	
-	// Draw scroll bar at bottom
-	drawRectangle(ctx, "#9999ff", GRAPHMARGINX, GRAPHSIZE-GRAPHMARGINX*2, GRAPHSIZE-GRAPHMARGINY, 24);
-	drawRectangle(ctx, GRAPHBACKGROUND, GRAPHMARGINX+BORDERTHICKNESS, GRAPHSIZE-(GRAPHMARGINX*2)-(BORDERTHICKNESS*2), GRAPHSIZE-GRAPHMARGINY+BORDERTHICKNESS, 16);
-	drawRectangle(ctx, "#9999ff", GRAPHMARGINX+scrollPosX, GRAPHMARGINX+SCROLLBARX, GRAPHSIZE-GRAPHMARGINY, 24);
+	// Border rectangles, because sometimes lines and points stretch outside the graph but still need to be rendered.
+	drawRectangle(ctx, GRAPHBACKGROUND, 0, size, 0, GRAPHMARGINY);
+	drawRectangle(ctx, GRAPHBACKGROUND, 0, size, GRAPHSIZE-GRAPHMARGINY, GRAPHMARGINY);
+	
+	// This code is a crime against humanity. But alas, there may be no better way of doing this - excluding the utilization of wacky maths that I don't know how to do.
+	// Vertical line labels
+	for (var i = Math.ceil(metaData[0]); i < metaData[1]; i += 1)
+	{
+		if (i % vertLinesDistance == 0) {
+			ctx.fillStyle = "#ffffff";
+			ctx.font = "10px Monospace";
+			ctx.fillText(i, stretchNum(i, metaData[0], metaData[1], GRAPHMARGINX, GRAPHSIZE-GRAPHMARGINX) - (i.toString().length)*3, GRAPHMARGINY-5); 
+		}
+	}
+	
+	// Horizontal line labels
+	for (var i = Math.ceil(metaData[2]); i < metaData[3]; i += 1)
+	{		
+		if (i % horizLinesDistance == 0) {
+			ctx.fillStyle = "#ffffff";
+			ctx.font = "10px Monospace";
+			ctx.fillText(i, GRAPHSIZE-GRAPHMARGINX+(i.toString().length)+3, stretchNum(i, metaData[2], metaData[3], GRAPHMARGINY, GRAPHSIZE-GRAPHMARGINY)+3);
+		}
+	}
+	
+	// Draw zoom bar at bottom
+	drawRectangle(ctx, riskColour, GRAPHMARGINX, GRAPHSIZE-GRAPHMARGINX*2, GRAPHSIZE-GRAPHMARGINY, SCROLLBARY);
+	drawRectangle(ctx, GRAPHBACKGROUND, GRAPHMARGINX+BORDERTHICKNESS, GRAPHSIZE-GRAPHMARGINX*2-BORDERTHICKNESS*2, GRAPHSIZE-GRAPHMARGINY+BORDERTHICKNESS, SCROLLBARY-BORDERTHICKNESS*2);
+	drawRectangle(ctx, riskColour, GRAPHMARGINX+scrollPosX, GRAPHMARGINX+SCROLLBARX, GRAPHSIZE-GRAPHMARGINY, SCROLLBARY);
+	
+	// Draw zoom bar on left
+	drawRectangle(ctx, riskColour, GRAPHMARGINX-SCROLLBARY, SCROLLBARY, GRAPHMARGINY, GRAPHSIZE-GRAPHMARGINY*2);
+	drawRectangle(ctx, GRAPHBACKGROUND, GRAPHMARGINX-SCROLLBARY+BORDERTHICKNESS, SCROLLBARY-BORDERTHICKNESS*2, GRAPHMARGINY+BORDERTHICKNESS, GRAPHSIZE-GRAPHMARGINY*2-BORDERTHICKNESS*2);
+	drawRectangle(ctx, riskColour, GRAPHMARGINX-SCROLLBARY, SCROLLBARY, GRAPHMARGINY+scrollPosY, GRAPHMARGINY+SCROLLBARX);
+	
+	// Draw zoom bar direction guides
+	ctx.fillStyle = riskColour;
+	ctx.font = "40px Monospace";
+	ctx.fillText("+", GRAPHMARGINX-SCROLLBARY-1, GRAPHSIZE-GRAPHMARGINY+25); 
+	ctx.fillText("-", GRAPHMARGINX-SCROLLBARY, GRAPHMARGINY-2); 
+	ctx.fillText("-", GRAPHSIZE-GRAPHMARGINX, GRAPHSIZE-GRAPHMARGINY+23); 	
 }
 
 
@@ -184,21 +228,25 @@ function recieveData() // recieve data from server
 	Data.push([Math.random()*0.5+Data.length+0.5, Math.random()*20-10]);
 	Data.push([Math.random()*0.5+Data.length+0.5, Math.random()*20-10]);
 	
-	drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, minY, maxY);
+	drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, -scrollValueY, scrollValueY);
 }
 
 function onLoad() {
 	Graph = document.getElementById("graph");
 	GraphContext = Graph.getContext("2d");
 	
-	drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, minY, maxY);
+	drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, -scrollValueY, scrollValueY);
 }
 
 
 
 function graphScrollHitboxX(x, y)
 {
-	return ( (x>GRAPHMARGINX) && (x<GRAPHSIZE-GRAPHMARGINX) && (y>GRAPHSIZE-GRAPHMARGINY+12) && (y<GRAPHSIZE-GRAPHMARGINY+36) );
+	return ( (x>GRAPHMARGINX) && (x<GRAPHSIZE-GRAPHMARGINX) && (y>GRAPHSIZE-GRAPHMARGINY/2) && (y<GRAPHSIZE-GRAPHMARGINY+SCROLLBARY*1.5) ); // Not sure why the * 1.5 and the * 0.5 since I found it through trial and error but it works so I'll leave it for now
+}
+function graphScrollHitboxY(x, y)
+{
+	return ( (x>GRAPHMARGINX-SCROLLBARY*1.5) && (x<GRAPHMARGINX+SCROLLBARY/2) && (y>GRAPHMARGINY) && (y<GRAPHSIZE-GRAPHMARGINY) );
 }
 
 var graphDraggingX = false;
@@ -206,29 +254,41 @@ var graphDraggingY = false;
 function graphMouseDown(event) {
 	if (graphScrollHitboxX(event.clientX, event.clientY)) {	
 		graphDraggingX = true;
-		graphMouseMove(event)
+		graphMouseMove(event);
+	} else if (graphScrollHitboxY(event.clientX, event.clientY)) {	
+		graphDraggingY = true;
+		graphMouseMove(event);
 	}
 }
 
 function graphMouseUp(event) {
 	graphDraggingX = false;
+	graphDraggingY = false;
 }
 
-const SCROLLMINX = 4;
-const SCROLLMAXX = 128;
+const SCROLLMINX = 5;
+const SCROLLMAXX = 150;
 
-const SCROLLBARX = 32; // Size of the X axis scroll bar in pixels
+const SCROLLMINY = 5;
+const SCROLLMAXY = 120;
 
-var minY = -40;
+const SCROLLBARX = 32; // Size of scroll bars (parallel to nearest graph edge)
+const SCROLLBARY = 24; // Size of scroll bars (perpendicular to nearest graph edge)
+
+var minY = -40; // these do nothing right now, but they will be necessary if i decide to let user navigate graph freely
 var maxY = 40;
+
 var scrollPosX = 0;
-var scrollValueX = 4;
+var scrollValueX = SCROLLMINX;
+
+var scrollPosY = 0;
+var scrollValueY = SCROLLMAXY;
+
 function graphMouseMove(event) {
 	if (graphDraggingX)
 	{
 		scrollPosX = event.clientX - GRAPHMARGINX - SCROLLBARX;
 		var scrollPosXMax = GRAPHSIZE - GRAPHMARGINX*2 - SCROLLBARX*2;
-		console.log(scrollPosX);
 		if (scrollPosX < 0)
 		{
 			scrollPosX = 0;
@@ -238,7 +298,21 @@ function graphMouseMove(event) {
 		}
 		
 		scrollValueX = stretchNum(scrollPosX, 0, GRAPHSIZE-GRAPHMARGINX, SCROLLMINX, SCROLLMAXX);
-		drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, minY, maxY);
+		drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, -scrollValueY, scrollValueY);
+	} else if (graphDraggingY) 
+	{
+		scrollPosY = event.clientY - GRAPHMARGINY - SCROLLBARX;
+		var scrollPosYMax = GRAPHSIZE - GRAPHMARGINY*2 - SCROLLBARX*2;
+		if (scrollPosY < 0)
+		{
+			scrollPosY = 0;
+		} else if (scrollPosY > scrollPosYMax)
+		{
+			scrollPosY = scrollPosYMax;
+		}
+		
+		scrollValueY = stretchNum(scrollPosYMax-scrollPosY, 0, GRAPHSIZE-GRAPHMARGINY, SCROLLMINY, SCROLLMAXY);
+		drawGraph(Graph, GraphContext, "#ffffff", GRAPHSIZE, Data, scrollValueX, -scrollValueY, scrollValueY);
 	}
 }
 
